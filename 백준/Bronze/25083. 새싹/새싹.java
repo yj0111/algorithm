448fc393f2ed74ba0c06fd4b21d32defabc3c8c4
yj0111 <118320449+yj0111@users.noreply.github.com>
2023-01-13 22:34:56 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+        
+	}
+}
