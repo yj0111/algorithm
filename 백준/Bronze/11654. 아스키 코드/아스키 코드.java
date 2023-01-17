e5492142548feee05160338f9ef2c07c4f9f5d13
Youjung <118320449+yj0111@users.noreply.github.com>
2023-01-17 23:06:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int ch = in.next().charAt(0);
+        
+		System.out.print(ch);
+	}
+}
