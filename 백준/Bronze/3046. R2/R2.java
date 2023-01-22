5b80555835452a004091f79c4d97090fa642f78d
Youjung <118320449+yj0111@users.noreply.github.com>
2023-01-22 12:33:21 +0900

    [Bronze IV] Title: R2, Time: 208 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R1 = sc.nextInt();
+		int S = sc.nextInt();
+		int R2= (2*S)-R1;
+
+		System.out.println(R2);
+		
+				
+	
+	}
+
+}
