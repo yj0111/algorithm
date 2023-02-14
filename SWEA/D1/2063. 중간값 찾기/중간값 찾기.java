c71f4d04b1a8826cbd166c05e8b81d2193d55946
Youjung <118320449+yj0111@users.noreply.github.com>
2023-02-14 15:40:40 +0900

    [D1] Title: 중간값 찾기, Time: 155 ms, Memory: 20,976 KB -BaekjoonHub

---
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] list = new int[N];
+		for(int i = 0; i<N; i++) {
+			list[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(list);
+		
+		System.out.println(list[N/2]);
+	}
+}
