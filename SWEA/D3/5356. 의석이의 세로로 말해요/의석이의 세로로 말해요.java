7f71336240d9833b5d1d2d7c9b00baf0b65f869e
Youjung <118320449+yj0111@users.noreply.github.com>
2023-02-20 16:38:02 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 163 ms, Memory: 23,696 KB -BaekjoonHub

---
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//테스트 케이스 T
+		int T = sc.nextInt();
+		
+		for(int i = 0; i<T; i++) {
+			//총 5줄로 이루어짐
+			
+				Stack<Object> stack1 = new Stack<>();
+				String str = sc.next();
+				for(int k = 0; k<str.length(); k++) {
+					stack1.push(str.substring(k, k+1));
+				}
+
+				Stack<Object> stack2 = new Stack<>();
+				String str2 = sc.next();
+				for(int k = 0; k<str2.length(); k++) {
+					stack2.push(str2.substring(k, k+1));
+				}
+
+				Stack<Object> stack3 = new Stack<>();
+				String str3 = sc.next();
+				for(int k = 0; k<str3.length(); k++) {
+					stack3.push(str3.substring(k, k+1));
+				}
+
+				Stack<Object> stack4 = new Stack<>();
+				String str4 = sc.next();
+				for(int k = 0; k<str4.length(); k++) {
+					stack4.push(str4.substring(k, k+1));
+				}
+				
+				Stack<Object> stack5 = new Stack<>();
+				String str5 = sc.next();
+				for(int k = 0; k<str5.length(); k++) {
+					stack5.push(str5.substring(k, k+1));
+				}
+				
+				int max = Math.max(Math.max(Math.max(Math.max(str.length(), str2.length()),str3.length()),str4.length()),str5.length());
+				
+				StringBuilder sb = new StringBuilder();
+
+				while(!(stack1.isEmpty()&&stack2.isEmpty()&&stack3.isEmpty()&&stack4.isEmpty()&&stack5.isEmpty())){
+					if(max == stack5.size()) {
+						sb.append(stack5.pop());
+					}
+					if(max == stack4.size()) {
+						sb.append(stack4.pop());
+					}
+					if(max == stack3.size()) {
+						sb.append(stack3.pop());
+					}
+					if(max == stack2.size()) {
+						sb.append(stack2.pop());					
+					}
+					if(max == stack1.size()) {
+						sb.append(stack1.pop());					
+					}
+					max--;
+				}
+				System.out.print("#"+(i+1)+ " "+ sb.reverse()+"\n");
+				
+				
+		}
+		
+		
+	}
+
+}
