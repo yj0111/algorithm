b3617a24248e7f9f9efc750d69bd7e80159716a7
Youjung <118320449+yj0111@users.noreply.github.com>
2023-03-03 17:34:02 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 264 ms, Memory: 27404 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//총 사람수 
+		int N = sc.nextInt();
+		//몇번째?
+		int K = sc.nextInt();
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		//사람들 넣기
+		for(int i = 1; i<=N; i++) {
+			q.offer(i);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        //사이즈 1 될때까지 반복
+		while(q.size() != 1) {
+			for(int i = 0; i<K-1; i++) {
+				//K번째 전까지는 뒤로 넣기
+				q.offer(q.poll());
+			}
+			//K번째인거 꺼내기
+		sb.append(q.poll()+", ");
+		}
+		
+		//1개 남으면 마지막에 꺼내기
+		sb.append(q.poll()+">");
+		
+		System.out.println(sb);
+	}
+}
