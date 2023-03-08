015fd7b798e54fce8a051ec916a0b93d928a70a4
Youjung <118320449+yj0111@users.noreply.github.com>
2023-03-08 17:04:31 +0900

    Update Hashing.java

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,6 +1,8 @@
+package bj;
+
 import java.util.Scanner;
 
-public class Main {
+public class hashing {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		//문자의 길이
@@ -9,16 +11,19 @@ public class Main {
 		String text = sc.next();
 		
 		//거듭제곱 곱한거의 합
-		int sum = 0;
+		long sum = 0;
+		long r = 1;
+		int M = 1234567891;
 		
 		//Math.pow(a,b) 가 a의 b 거듭 제곱이래
+		//Math.pow 쓰면 long 범위를 벗어나서 50 점 밖에 안된대요
+		
+		//그냥 매번 곱해주면서 더하기 
 		for (int i = 0; i < L; i++) {
-			int a = text.charAt(i)-'a'+1;
-			double result = Math.pow(31, i);
-			sum += a * result;
+			sum+= ((text.charAt(i)-'a'+1)*r)%M;
+			r = (r*31)%M;
 		}
 		
-		int M = 1234567891;
 		System.out.println(sum%M);
 		
 	}
