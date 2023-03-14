365d1542ca187a604a6b99cfbdafdd2de9181442
Youjung <118320449+yj0111@users.noreply.github.com>
2023-03-14 13:27:38 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 144 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::"+ str +"::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}
