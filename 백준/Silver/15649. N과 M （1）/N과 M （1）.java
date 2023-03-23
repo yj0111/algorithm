057d73535b996d30a568d3d6ba8be20f38157733
Youjung <118320449+yj0111@users.noreply.github.com>
2023-03-23 14:13:27 +0900

    [Silver III] Title: N과 M (1), Time: 2964 ms, Memory: 77336 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] list;
+	static int[] result;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		list = new int[N + 1];
+		result = new int[N + 1]; // 결과 순열
+		visited = new boolean[N + 1];
+
+		// list 에 1~ N까지 담기
+		for (int i = 1; i <= N; i++) {
+			list[i] = i;
+		}
+
+		check(0);
+
+	}
+
+	public static void check(int idx) {
+		if (idx == M) {
+			for (int i = 0; i < result.length; i++) {
+				if (result[i] != 0) {
+					System.out.print(result[i] + " ");
+				}
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			if (visited[i]) {
+				continue;
+			}
+			result[idx] = list[i];
+			visited[i] = true;
+
+			check(idx + 1);
+			visited[i] = false;
+
+		}
+	}
+}
