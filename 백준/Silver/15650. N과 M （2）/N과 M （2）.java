7fe36780116d54d2507b8a21cf8f80fb85caedef
Youjung <118320449+yj0111@users.noreply.github.com>
2023-03-24 12:24:25 +0900

    [Silver III] Title: N과 M (2), Time: 132 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] list;
+	static int[] result;
+	static boolean[] visited;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		list = new int[M];
+		visited = new boolean[N + 1];
+
+		check(1, 0);
+		bw.flush();
+	}
+
+	//at : 시작점
+	public static void check(int at, int idx) throws IOException {
+		if (idx == M) {
+			for (int i = 0; i < M; i++) {
+				bw.append(String.valueOf(list[i]) + " ");
+			}
+			bw.append("\n");
+			return;
+		}
+
+		for (int i = at; i <= N; i++) {
+			list[idx] = i;
+			check(i + 1, idx + 1);
+		}
+
+	}
+}
