ca9ce795ed4f7fdb337371a0e5333add239a3461
Youjung <118320449+yj0111@users.noreply.github.com>
2023-03-25 22:50:02 +0900

    [Silver III] Title: N과 M (5), Time: 2500 ms, Memory: 67428 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] list;
+	static int[] result;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		list = new int[N + 1];
+		result = new int[M]; // 결과 순열
+		visited = new boolean[N + 1];
+
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		// list 에 1~ N까지 담기
+		for (int i = 1; i <= N; i++) {
+			list[i] = Integer.parseInt(st1.nextToken());
+		}
+		Arrays.sort(list);
+		check(0);
+
+	}
+
+	public static void check(int idx) {
+		if (idx == M) {
+			for (int i = 0; i < M; i++) {
+				if (result[i] != 0) {
+					System.out.print(result[i] + " ");
+				}
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			if (visited[i]) {
+				continue;
+			}
+			result[idx] = list[i];
+			visited[i] = true;
+
+			check(idx + 1);
+			visited[i] = false;
+
+		}
+	}
+}
