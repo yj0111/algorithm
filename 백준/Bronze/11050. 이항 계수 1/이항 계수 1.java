7d734090952a9de4fa30edfa0e6e617483abe09e
Youjung <118320449+yj0111@users.noreply.github.com>
2023-03-27 17:30:10 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		//             n! 
+		// nCk = -------------
+		//       ((n-k)! * k!)
+		System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
+	}
+
+	static int factorial(int N) {
+		if (N <= 1) {
+			return 1;
+		}
+		return N * factorial(N - 1);
+	}
+}
