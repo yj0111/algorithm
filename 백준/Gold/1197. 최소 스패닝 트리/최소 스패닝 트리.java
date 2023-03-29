a46251cd88e093b5590f8c1f8940409ad6f5eeb3
Youjung <118320449+yj0111@users.noreply.github.com>
2023-03-29 21:02:28 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 560 ms, Memory: 49972 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int V;
+	static int E;
+	static int[] p;
+	static int[][] edges;
+
+	//최소 스패닝 트리
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		V = Integer.parseInt(st.nextToken());
+
+		E = Integer.parseInt(st.nextToken());
+
+		edges = new int[E][3];
+
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			edges[i][0] = Integer.parseInt(st.nextToken());
+			edges[i][1] = Integer.parseInt(st.nextToken());
+			edges[i][2] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(edges, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[2] - o2[2];
+			}
+		});
+
+		p = new int[V + 1];
+		makeset();
+
+		long result = 0;
+		for (int i = 0; i < E; i++) {
+			if (union(edges[i][0], edges[i][1])) {
+				result += edges[i][2];
+			}
+		}
+
+		System.out.println(result);
+	}
+
+	static void makeset() {
+		for (int i = 1; i <= V; i++) {
+			p[i] = i;
+		}
+	}
+
+	static int findset(int a) {
+		if (p[a] == a) {
+			return a;
+		} else {
+			return p[a] = findset(p[a]);
+		}
+	}
+
+	static boolean union(int a, int b) {
+		int aRoot = findset(a);
+		int bRoot = findset(b);
+
+		if (aRoot != bRoot) {
+			p[bRoot] = aRoot;
+			return true;
+		}
+		return false;
+	}
+}
