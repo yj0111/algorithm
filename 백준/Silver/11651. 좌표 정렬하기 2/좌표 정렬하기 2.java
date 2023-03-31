ec065d6d29713707369bc18ac6a9410e3ae1a1e6
Youjung <118320449+yj0111@users.noreply.github.com>
2023-03-31 16:03:41 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 860 ms, Memory: 62008 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+
+		int[][] map = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			map[i][0] = Integer.parseInt(st.nextToken());
+			map[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(map, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				}
+				return o1[1] - o2[1];
+			}
+		});
+		;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 2; j++) {
+				sb.append(map[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
