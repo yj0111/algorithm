60dd190bd8aaca88ac7fee607d812d218ed2a643
Youjung <118320449+yj0111@users.noreply.github.com>
2023-04-15 18:36:44 +0900

    [Bronze II] Title: 다이얼, Time: 200 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+		
+		String s = in.nextLine();
+ 
+		int count = 0;
+		int k = s.length();
+        
+		for(int i = 0; i < k; i++) {
+        
+			switch(s.charAt(i)) {
+            
+			case 'A' : case 'B': case 'C' : 
+				count += 3;
+				break;
+                
+			case 'D' : case 'E': case 'F' :
+				count += 4; 
+				break;
+                
+			case 'G' : case 'H': case 'I' :
+				count += 5; 
+				break;
+                
+			case 'J' : case 'K': case 'L' : 
+				count += 6;
+				break;
+                
+			case 'M' : case 'N': case 'O' :
+				count += 7;
+				break;
+                
+			case 'P' : case 'Q': case 'R' : case 'S' : 
+				count += 8; 
+				break;
+                
+			case 'T' : case 'U': case 'V' : 
+				count += 9;
+				break;
+                
+			case 'W' : case 'X': case 'Y' : case 'Z' : 
+				count += 10;
+				break;
+			}
+		}		
+		System.out.print(count);
+	}
+}
