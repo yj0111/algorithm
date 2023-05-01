47dcc6a8aff1a18d6c8313671ffa60cbfdd0233f
Youjung <118320449+yj0111@users.noreply.github.com>
2023-05-01 16:54:08 +0900

    [Gold V] Title: 택배 배송, Time: 944 ms, Memory: 136704 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -3,74 +3,70 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
-    static int N, M;
-    static ArrayList<Node>[] graph;
-    static int[] dist;
-    static boolean[] visited;
+	static int N, M;
+	static boolean[] visited;
+	static int[] dp;
+	static ArrayList<Node>[] graph;
 
-    static class Node implements Comparable<Node> {
-        int index, weight;
+	static class Node implements Comparable<Node> {
+		int index;
+		int value;
 
-        Node(int index, int weight) {
-            this.index = index;
-            this.weight = weight;
-        }
+		Node(int index, int value) {
+			this.index = index;
+			this.value = value;
+		}
 
-        public int compareTo(Node o) {
-            return weight - o.weight;
-        }
-    }
+		@Override
+		public int compareTo(Node o) {
+			return this.value - o.value;
+		}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+	}
 
-        graph = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		visited = new boolean[N + 1];
+		dp = new int[N + 1];
+		for (int i = 2; i <= N; i++) {
+			dp[i] = Integer.MAX_VALUE;
+		}
+		graph = new ArrayList[N + 1];
+		for (int n = 0; n <= N; n++) {
+			graph[n] = new ArrayList<>();
+		}
+		for (int m = 0; m < M; m++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			graph[a].add(new Node(b, c));
+			graph[b].add(new Node(a, c));
+		}
+		// 기본 셋팅 완료.
 
-        for (int i = 0; i < M; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            graph[a].add(new Node(b, c));
-            graph[b].add(new Node(a, c));
-        }
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(1, 0));
 
-        dist = new int[N+1];
-        visited = new boolean[N+1];
-        for (int i = 1; i <= N; i++) {
-            dist[i] = Integer.MAX_VALUE;
-        }
-
-        // 다익스트라 알고리즘 시작
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(1, 0));
-        dist[1] = 0;
-
-        while (!pq.isEmpty()) {
-            Node curNode = pq.poll();
-            int curIndex = curNode.index;
-
-            if (visited[curIndex]) {
-                continue;
-            }
-
-            visited[curIndex] = true;
-
-            for (Node nextNode : graph[curIndex]) {
-                int nextIndex = nextNode.index;
-                int nextWeight = nextNode.weight;
-
-                if (!visited[nextIndex] && dist[nextIndex] > dist[curIndex] + nextWeight) {
-                    dist[nextIndex] = dist[curIndex] + nextWeight;
-                    pq.add(new Node(nextIndex, dist[nextIndex]));
-                }
-            }
-        }
-
-        System.out.println(dist[N]);
-    }
+		
+		
+		
+		while (!pq.isEmpty()) {
+			Node node = pq.poll();
+			if(visited[node.index]) {
+				continue;
+			}
+			visited[node.index]=true;
+			for (Node nodeTmp : graph[node.index]) {
+				if (!visited[nodeTmp.index]&&dp[nodeTmp.index]>dp[node.index]+nodeTmp.value) {
+						dp[nodeTmp.index]=dp[node.index]+nodeTmp.value;
+						pq.add(new Node(nodeTmp.index, dp[nodeTmp.index]));
+						
+					
+				}
+			}
+		}
+		System.out.println(dp[N]);
+	}
 }
