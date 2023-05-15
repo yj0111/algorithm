d712862f946809f6af4879010610ee15858fe2bd
Youjung <118320449+yj0111@users.noreply.github.com>
2023-05-15 10:57:20 +0900

    [Bronze V] Title: 이상한 기호, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		System.out.println((a + b) * (a - b));
+	}
+}
