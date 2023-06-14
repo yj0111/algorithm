fff51135f37af45c621a7fe8eaf5569b355ff158
Youjung <118320449+yj0111@users.noreply.github.com>
2023-06-14 22:02:49 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.14 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = myString.toLowerCase();
+        
+        System.out.print(answer);
+        return answer;
+    }
+}
