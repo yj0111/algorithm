f4843654aeecf11d83a13ff94b637dbfe52d7b01
Youjung <118320449+yj0111@users.noreply.github.com>
2023-07-03 15:18:06 +0900

    [Silver IV] Title: 차집합, Time: 4180 ms, Memory: 206008 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.java
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -1,37 +1,35 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int[] arrA = new int[a];
+        int[] arrB = new int[b];
 
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		
-		//집합 A의 원소 개수 
-		int A = sc.nextInt();
-		
-		//집합 B의 원소 개수 
-		int B = sc.nextInt();
+        TreeSet<Integer> aSet = new TreeSet<>();
 
-		TreeSet<Integer> A_set = new TreeSet<Integer>();
-		
-		for(int i = 0; i <A; i++) {
-			A_set.add(sc.nextInt());
-		}
-		
-		for(int i = 0; i <B; i++) {
-			int addB = sc.nextInt();
-			//B가 있으면 지워버리기
-			if(A_set.contains(addB)) {
-				A_set.remove(addB);
-			}
-		}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < a; i++) {
+            aSet.add(Integer.parseInt(st.nextToken()));
+        }
 
-		System.out.println(A_set.size());
-		
-		//설마 for문 안에 int로 해서 틀리나 ??
-		for(Integer x : A_set) {
-			System.out.print(x+" ");
-		}
-	}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < b; i++) {
+            int k = Integer.parseInt(st.nextToken());
+            if (aSet.contains(k))
+                aSet.remove(k);
+        }
+
+        System.out.println(aSet.size());
+        for (int x : aSet) {
+            System.out.print(x + " ");
+        }
+    }
 }
