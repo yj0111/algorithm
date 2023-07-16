0189c23a6159e2c342d49532a17f1d56ce23f64d
Youjung <118320449+yj0111@users.noreply.github.com>
2023-07-16 23:06:13 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        int len = phone_number.length();
+        for(int i = 0; i<len-4; i++){
+            answer+="*";
+        }
+        
+        answer += phone_number.substring(len-4,len);
+
+        return answer;
+    }
+}
