2110192bcd624ef4c802f228d6a95958af21a847
Youjung <118320449+yj0111@users.noreply.github.com>
2023-08-06 22:26:59 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.46 ms, Memory: 51.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 2;
+        
+        for (int i = 2; i < n; i++) {
+            int num = arr[i - 1] + arr[i - 2];
+            arr[i] = num % 1000000007;
+        }
+        
+        answer = arr[n-1];
+        return answer;
+    }
+}
