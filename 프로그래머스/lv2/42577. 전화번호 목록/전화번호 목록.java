0e18e42f51bbf3be1e2ffc4cb6d52ce316a8ff6a
Youjung <118320449+yj0111@users.noreply.github.com>
2023-09-27 01:37:36 +0900

    [level 2] Title: 전화번호 목록, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -14,12 +14,9 @@ class Solution {
     }
 }
 
-/* 틀린 이유 (1) contains
+/* 틀린 이유 contains
     if (phone_book[i + 1].contains(phone_book[i])) 
     contains 로 해서 13번 테스트 케이스만 틀렸는데 
     문제 조건이 "접두사"인 경우만 이라서 contains 쓰면 안됨 ! 
     -> startswith로 다시 해봄
-    
-    틀린이유 (2) startsWith
-    -> 모든 테스트 케이스 런타임 에러 
 */
