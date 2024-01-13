24e5048d76e0032529cdfa0989d371e8cdd27714
Youjung <118320449+yj0111@users.noreply.github.com>
2024-01-14 01:43:11 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1636 ms, Memory: 59820 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		long[] S = new long[N+1];
-		
-		st = new StringTokenizer(bufferedReader.readLine());
-		
-		//누적합
-		for(int i = 1; i<=N; i++) {
-			S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
-		}
-		
-		for(int j = 0; j < M; j++) {
-			st = new StringTokenizer(bufferedReader.readLine());
-			int k = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
-			System.out.println(S[m] - S[k-1] );	
-		}
-	}
+        int N = Integer.parseInt(st.nextToken());// 수의 개수
+        int M = Integer.parseInt(st.nextToken());// 합을 구해야하는 횟수
+
+        long[] prefix_sum = new long[N+1]; // 누적합 배열 만들기
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 1; i<=N; i++) { //그냥 첫번째는 0으로 처리하기
+            prefix_sum[i] = prefix_sum[i-1]+Integer.parseInt(st.nextToken());
+        }
+
+        //a번째에서 b번째까지의 합이니까 누적합 배열에서 b 번째 - (a-1)번쨰
+        for(int j = 0; j<M; j++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(prefix_sum[b]-prefix_sum[a-1]);
+        }
+    }
 }
